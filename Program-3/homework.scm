26d41b1c8f97a96288cbd25fa46b43a64a5e41c2
Abdalla2024 <abdallaa2024@gmail.com>
2025-04-08 21:38:48 -0400

    Completed Task 2

--- Program-3/homework.scm
+++ Program-3/homework.scm
@@ -12,3 +12,22 @@
         0                       ; Return 0
         (+ 1 (quotient (- end first-divisible) div))))) ; Count how many divs fit in range
 
+; Rewriting above to accept predicate function
+(define (count-predicate low high pred)
+  (let* ((start (min low high))    
+         (end (max low high)))
+
+    ; Helper function to loop through range
+    (define (helper current count)
+      (cond
+        ((> current end) count) ; If you pass end, return count
+        ((pred current) (helper (+ current 1) (+ count 1))) ; If pred is true, increment count
+        (else (helper (+ current 1) count))))
+
+      (helper start 0)))
+
+(write (count-div 1 12 4))
+(newline)
+
+(write (count-predicate 1 12 (lambda (a) (= (modulo a 3) 0))))
+(newline)
